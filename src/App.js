a058f6c51a297a1ff75c59265d1e87e94f83b33a
Ollie Stevenson <ollie@stevenson.digital>
2019-08-18 15:17:10 +0100

    [remove] default css and logos

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import fire from './config/Fire'
 import Login from './components/Login'
 import Home from './components/Home'
 
-import './App.css'
-
 class App extends Component {
   constructor(props) {
     super(props)
